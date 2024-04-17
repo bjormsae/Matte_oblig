6f7919b26de85bea37b0555acfa480a009273e90
bjormsae <160119259+bjormsae@users.noreply.github.com>
2024-04-17 11:04:22 +0200

    la til kommentarer

--- heat_equation_implicit2D.py
+++ heat_equation_implicit2D.py
@@ -5,8 +5,11 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 from numpy import linalg
 import time
+# Implisitt løsning av varmelikningen i to dimensjoner. Viste seg at jeg fikk til mer oppløsning på resultatet med denne løsningen. 
+# Det kan ha noe med at jeg ikke fikk til å implementere parallell-kjøring av den eksplisitte metoden. Den store utfordingen med denne 
+# framgangsmåten var alogritmen for å lage matrisen. Har lagt ved div kommentarer på spennende steder.
 
-def createMatrixBroken(size, k): # fun funciton but does not produce the correct matrix
+def createMatrixBroken(size, k): # første forsøk på å lage matrise. Funka ikke.
     arr = []
     for t in range(0, size):
         arr.append([])
@@ -33,39 +36,41 @@ def createMatrixBroken(size, k): # fun funciton but does not produce the correct
     return np.array(arr)
 
 #------------------------------------------------
-#------------------------------------------------ code below
+#------------------------------------------------ kode begynner her.
 def createMatrix(size, k):
-    helpp = []
+    helpp = []  # lage hjelpematrise av størrelse size * size. 
     for n in range(0, size):
         helpp.append([])
         for nn in range(0, size):
             helpp[n].append(nn+ size*n)
 
-    arr = []
+    arr = []    # lage faktisk matrise av størrelse size^2 * size^2. size^2 er antall piksler i beregningen vår så denne blir umennesklig
+                # stor veldig fort. 
     s = size*size
     for y in range(0, s):
         arr.append([])
-        b = findNeighbours(y, helpp, size)
+        b = findNeighbours(y, helpp, size) # hjelpefunksjon som returnerer array med posisjonene i arr som skal settes til -k. 
 
         for x in range(0, s):
-            if (x in b):
+            if (x in b): # sjekker om skal settes til -k eller 0.
                 arr[y].append(-k)
                 continue
             arr[y].append(0)
-            # walking across a x-file
+            # går langs x-linja
         arr[y][y] = 4*k +1
 
     return np.array(arr)
 
 
-def findNeighbours(x, helpMatrix, size):
+def findNeighbours(x, helpMatrix, size): # Finner naboer i hjelpematrisen og forteller oss hvor de vil legge seg i den store matrisen
+                        # Hele hjelpematrisen blir til en enkelt rad i den store, og hver pixel representeres av sine naboer i en rad.
     arr = []
     xxx = x - x%size
 
     x_comp = x - xxx
-    y_comp = int(xxx/size)
+    y_comp = int(xxx/size) # tror dette var noe omgjøring mellom koordinater. 
 
-    if (x_comp != 0 and x_comp != len(helpMatrix)-1 and y_comp != 0 and y_comp != len(helpMatrix)-1):
+    if (x_comp != 0 and x_comp != len(helpMatrix)-1 and y_comp != 0 and y_comp != len(helpMatrix)-1): # lite elegant, men funker
         arr.append(helpMatrix[y_comp-1][x_comp])
         arr.append(helpMatrix[y_comp+1][x_comp])
         arr.append(helpMatrix[y_comp][x_comp-1])
@@ -101,7 +106,7 @@ def findNeighbours(x, helpMatrix, size):
 
     return arr
 
-def solveHeatEquationImplicit(init):
+def solveHeatEquationImplicit(init): # tar inn initialmatrise
     iterations = 350
     x_nodes = len(init)
     dx = 1/x_nodes
@@ -110,13 +115,13 @@ def solveHeatEquationImplicit(init):
     k_const = dt/(dx*dx)  #  factor
 
     start_time = time.time()
-    A = createMatrix(x_nodes, k_const) # defines the matrix
+    A = createMatrix(x_nodes, k_const) # lager matrisen A
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Matrix created. Time used: ", elapsed_time)
 
     start_time = time.time()
-    inverse_A = linalg.inv(A)
+    inverse_A = linalg.inv(A) # Inverterer matrisen. Her tar ting lang tid, men sparer det igjen senere
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Matrix inverted. Time used: ", elapsed_time)
@@ -124,17 +129,17 @@ def solveHeatEquationImplicit(init):
     solution = []
     solution.append(np.array(init))
 
-    for k in range(0, iterations):   # solves the system of equations for each timestep and adds it to the solution
-        b = solution[k].flatten()   # flatten into a single array
+    for k in range(0, iterations):   # Her lages løsninger
+        b = solution[k].flatten()   # må gjøres fordi .dot() ikke er definert for n*n.
 
-        next_it = inverse_A.dot(b)
+        next_it = inverse_A.dot(b)    # i stedet for linalg.solve(A, b)
 
-        next_it = next_it.reshape(x_nodes, x_nodes)  # reshape back into nxn array
+        next_it = next_it.reshape(x_nodes, x_nodes)  # får tilbake orginal form
         solution.append(next_it)
         print(k/iterations*100, " % complete")
     solution = np.array(solution)
 
-        #-------------------------------- copy paste for creating this again
+        #-------------------------------- copy paste
     def plotheatmap(u_k, k):
         # Clear the current plot figure
         plt.clf()
@@ -152,13 +157,13 @@ def solveHeatEquationImplicit(init):
     def animate(k):
         plotheatmap(solution[k], k)
     #----------------------------------
-    # the gif is created here 
+    # gif
     anim = animation.FuncAnimation(plt.figure(), animate, interval=1, frames=iterations, repeat=False)
     anim.save("heat_equation_solution_implicit4.gif")
 
-matrix_size = 80
+matrix_size = 80 # ikke sett noe særlig høyere.
 
-# Generate a creative combination of patterns
+# Genererer startbetingelse. (chatGPT som fant på)
 x, y = np.meshgrid(np.linspace(0, 1, matrix_size), np.linspace(0, 1, matrix_size))
 
 gradient = x + y
